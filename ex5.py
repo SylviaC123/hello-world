7ca9845d7bc5547f83e6253d95d15fbc31764c73
SylviaC123 <Sycarignan@gmail.com>
2013-12-09 18:34:21 -0500

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,5 @@
+## This file uses exercises from Chapter 5 of "Learn Python the Hard Way."
+## A list of Python format characters is online at http://docs.python.org/2/library/stdtypes.html#string-formatting under "The conversion types are:"
+
+my_name = "Sylvia"
+print "Let's talk about %s." % my_name
