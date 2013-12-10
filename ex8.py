5312894815d56ba9290e6a05f7178efbf254c715
SylviaC123 <Sycarignan@gmail.com>
2013-12-09 21:13:25 -0500

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,7 @@
+## Exercise from Chapter 8 of "Learn Python the Hard Way."
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ('one', 'two', 'three', 'four')
+print formatter % (formatter, formatter)
+
