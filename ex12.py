6f99b53a9964874bffd08d1ebebb921666948aaa
SylviaC123 <Sycarignan@gmail.com>
2013-12-09 22:36:54 -0500

    Create ex12

---
+++ ex12.py
@@ -0,0 +1,7 @@
+## Exercise from Chapter 12 of "Learn Python the Hard Way."
+name = raw_input("Name? ")
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "%r, you are %r years old, %r inches tall and weigh %r pounds." % (name, age, height, weight)
