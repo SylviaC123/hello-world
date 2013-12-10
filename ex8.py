af79e08f949a954034d8ab6e6664d0d34ea05291
SylviaC123 <Sycarignan@gmail.com>
2013-12-09 21:21:51 -0500

    fixed syntax

--- ex8.py
+++ ex8.py
@@ -3,5 +3,5 @@ formatter = "%r %r %r %r"
 
 print formatter % (1, 2, 3, 4)
 print formatter % ('one', 'two', 'three', 'four')
-print formatter % (formatter, formatter)
+print formatter % (formatter, formatter, formatter, formatter)
 
